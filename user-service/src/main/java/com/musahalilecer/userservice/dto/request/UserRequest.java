6bc518836fdfcf4b9cc98aebde0cf7b8ac1f366f
musahalilecer <musahalilecer34@gmail.com>
2025-03-01 12:29:59 +0300

    starting user-service

---
+++ user-service/src/main/java/com/musahalilecer/userservice/dto/request/UserRequest.java
@@ -0,0 +1,14 @@
+package com.musahalilecer.userservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private Long id;
+    private String username;
+    private String password;
+}
---
+++ user-service/src/main/java/com/musahalilecer/userservice/model/User.java
@@ -0,0 +1,29 @@
+package com.musahalilecer.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
